2db566746ed7f0c2ed87ceb66738fcb8f45519be
Aii.Fachri <fachrisnr76@gmail.com>
2026-04-01 21:54:23 +0700

    Add initial Express server setup in app.js and create server.js file

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+//import express
+const express = require("express");
+// Membuat object express
+const app = express();
+/*
+Membuat routing.
+Method get menerima 2 params.
+*
+*
+*
+* Param 2 callback.
+Param 1 adalah endpoint.
+* Callback menerima object req dan res
+*/
+app.get("/", (req, res) => {
+res.send("Hello Express");
+});
+// Mendefinisikan port.
+app.listen(3000);
