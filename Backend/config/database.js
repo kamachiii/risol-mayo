e9417889206b578b287f81a82ed2a5b2d266d2ee
Fi'adn Fawzul Adzhim <fiadn.fawzul.adzhim@gmail.com>
2026-04-09 15:16:41 +0700

    config database

---
+++ Backend/config/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+// buat koneksi ke database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'risol_mayo'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+module.exports = db;
